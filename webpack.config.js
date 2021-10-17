060bd1b87f906000a840fa60a4eedf9f469ca889
Samuel Eyre <samuel.eyre@hotmail.fr>
2021-10-17 16:08:43 +0200

    fix: for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './script.js',
     output: {
         filename: 'script.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     // module: { // not working...
     //     rules: [
